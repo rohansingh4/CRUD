beac178ee93422ad262678440ce49ece32a7edbb
rohansingh4 <rohan.singh1104@gmail.com>
2021-05-09 14:24:08 +0530

    update and delete functionalities added

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description} {self.completed}>'
 
 #db.create_all()
-
+#Create and Read
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
   error = False
@@ -44,6 +44,33 @@ def create_todo():
       #return redirect(url_for('index'))                 -----------------------------------------Using standard method
       return jsonify(body)
 
+#Update
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
+#Delete
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
+
 @app.route('/')
 def index():
-    return render_template('index.html', data =Todo.query.all())
+    return render_template('index.html', data =Todo.query.order_by('id').all())
